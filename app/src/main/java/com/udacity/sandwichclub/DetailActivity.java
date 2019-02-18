5b3a1f8484fca36a31b1308882c38f8596b76bf0
Wiolka <wiolkast@gmail.com>
2019-02-18 18:52:18 +0100

    Initial project submission

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,17 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.json.JSONException;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,7 +25,7 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        final ImageView imageIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,19 +41,29 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
-            // Sandwich data unavailable
+        try {
+            Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+            if(sandwich == null) {
+                closeOnError();
+                return;
+            }
+            populateUI(sandwich);
+            Picasso.with(this)
+                    .load(sandwich.getImage())
+                    .into(imageIv, new Callback(){
+                        @Override
+                        public void onSuccess() {
+                        }
+                        @Override
+                        public void onError() {
+                            imageIv.setVisibility(View.GONE);
+                        }
+                    });
+            setTitle(sandwich.getMainName());
+        } catch (JSONException e){
             closeOnError();
             return;
         }
-
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -56,7 +71,34 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        TextView alsoKnownAsTextView = findViewById(R.id.also_known_tv);
+        TextView alsoKnownAsLabel = findViewById(R.id.also_known_label);
+        alsoKnownAsTextView.setText("");
+        if(sandwich.getAlsoKnownAs().size()!=0) {
+            for (String string : sandwich.getAlsoKnownAs()) {
+                alsoKnownAsTextView.append(string + "\n");
+            }
+            alsoKnownAsTextView.setText(alsoKnownAsTextView.getText().toString()
+                    .substring(0, alsoKnownAsTextView.getText().length() - 1));
+        } else {
+            alsoKnownAsTextView.setVisibility(View.GONE);
+            alsoKnownAsLabel.setVisibility(View.GONE);
+        }
+        TextView placeOfOriginTextView = findViewById(R.id.origin_tv);
+        TextView placeOfOriginLabel = findViewById(R.id.place_of_origin_label);
+        if(!sandwich.getPlaceOfOrigin().equals("")) {
+            placeOfOriginTextView.setText(sandwich.getPlaceOfOrigin());
+        } else {
+            placeOfOriginTextView.setVisibility(View.GONE);
+            placeOfOriginLabel.setVisibility(View.GONE);
+        }
+        TextView descriptionTextView = findViewById(R.id.description_tv);
+        descriptionTextView.setText(sandwich.getDescription());
+        TextView ingredientsTextView = findViewById(R.id.ingredients_tv);
+        ingredientsTextView.setText("");
+        for(String string: sandwich.getIngredients()){
+            ingredientsTextView.append(string + "\n");
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,41 @@
 package com.udacity.sandwichclub.utils;
 
+import android.text.TextUtils;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
-public class JsonUtils {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+        JSONObject root = new JSONObject(json);
+        JSONObject name = root.getJSONObject("name");
+        String mainName = name.getString("mainName");
+        List<String> alsoKnownAsList = new ArrayList<>();
+        JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+        if (!alsoKnownAsArray.isNull(0)) {
+            for (int i = 0; i < alsoKnownAsArray.length(); i++) {
+                alsoKnownAsList.add(alsoKnownAsArray.getString(i));
+            }
+        }
+        String placeOfOrigin = root.getString("placeOfOrigin");
+        String description = root.getString("description");
+        String image = root.getString("image");
+        JSONArray ingredientsArray = root.getJSONArray("ingredients");
+        List<String> ingredientsList = new ArrayList<>();
+        if (!ingredientsArray.isNull(0)) {
+            for (int i = 0; i < ingredientsArray.length(); i++) {
+                ingredientsList.add(ingredientsArray.getString(i));
+            }
+        }
+        return new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredientsList);
     }
 }
